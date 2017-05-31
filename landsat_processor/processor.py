b32333a847bedb35ff26c0f837ec8844ca2d685f
Aline Tavares <alaisgomes@gmail.com>
2017-05-31 10:21:01 -0300

    Fixed bugs and add comments

--- landsat_processor/processor.py
+++ landsat_processor/processor.py
@@ -1,17 +1,23 @@
-#from imagery.models import Scene, Image, LandsatGrade
 import os
 import subprocess
-import sys
 
 class LandsatColorComposition():
-    """ Class that process and create colored composition from Landsat list of files """
+    """ 
+    Class that process and create colored composition from Landsat list of files.
+    Receive path to save resulting file, file name and ordered file list to 
+    create composition.
+        
+    """
     def __init__(self, out_path, name, file_list, quiet=True):
         self.out_path   = out_path
         self.name       = name
-        self.file_list  = file_list
+        self.file_list  = file_list     # ordered file list to merge
         self.quiet      = quiet
 
     def _set_full_file_path(self):
+        """
+        Returns complete file path for rgb composition 
+        """
         if self.name.endswith(".TIF"):
             file_path = os.path.join(self.out_path, self.name)
         else:
@@ -19,8 +25,8 @@ class LandsatColorComposition():
 
         return (file_path)
 
-
     def create_composition(self):
+        """ Creates rgb composition using gdal_merge.py """
         file_path = self._set_full_file_path()
 
         if not self.quiet:
@@ -33,7 +39,7 @@ class LandsatColorComposition():
                                 PHOTOMETRIC=RGB -o {}".format(quiet, file_path)
 
         for file in self.file_list:
-            gdal_mergepy_command += " " + file
+            gdal_mergepy_command += " " + file["path"]
 
         subprocess.call(gdal_mergepy_command, shell=True)
 
